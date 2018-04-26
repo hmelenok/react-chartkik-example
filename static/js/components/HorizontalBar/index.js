274442e4e9edc908742d6222fd0e2f4e2b99fd18
Khmel Mykyta <hmelenok@gmail.com>
2018-04-26 12:00:01 +0300

    Add data with timeout

--- static/js/components/HorizontalBar/index.js
+++ static/js/components/HorizontalBar/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import {BarChart} from 'react-chartkick';
+import ReactChartkick, {BarChart} from 'react-chartkick';
+import Chart from 'chart.js';
+
+ReactChartkick.addAdapter(Chart);
 import {generateArray} from '../../data-layer/array-processors';
 import {size} from 'lodash';
 import './index.scss';
-
 
 export default class HorizontalBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: generateArray()
+      data: this.getRandomData()
     };
   }
 
+  getRandomData() {
+    return generateArray().map(({val, label}) => [label, val]);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.setState({data: this.getRandomData()}), 1000);
+  }
 
   render() {
-    return <BarChart data={[["Work", 32], ["Play", 1492]]} />;
+    return (
+      <BarChart
+        data={this.state.data}
+        refresh={1}
+        messages={{empty: 'No data'}}
+        width="100%"
+        height="470px"
+        decimal=","
+        colors={['#0a83cf']}
+      />
+    );
   }
 }
